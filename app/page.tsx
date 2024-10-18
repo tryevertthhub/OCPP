7349739322c93cad095a8467263a80842d8e6861
tryevertthhub <tryevertth@gmail.com>
2024-10-18 14:01:42 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Map from './components/Map';
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Device Registry</h1>
+
+            <Map />
+        </div>
+    );
+};
+
+export default Home;
